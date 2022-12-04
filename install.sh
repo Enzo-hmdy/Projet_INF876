395f6f53bf4ca8956890643c8768489e78794d56
ehoummad <enzo.hoummady@insa-cvl.fr>
2022-12-04 14:01:50 -0500

    des trucs

--- install.sh
+++ install.sh
@@ -6,6 +6,14 @@ mv multichaind multichain-cli multichain-util /usr/local/bin
 multichain-util create wikichain
 # edit "/home/node1/.multichain/wikichain/params.dat" and put every false to true with sed
 sed -i 's/false/true/g' /home/node1/.multichain/wikichain/params.dat
+multichaind wikichain -daemon
+multichain-cli wikichain create stream wikichain true
+#edit cron file to execute "grant_stream_post.sh" every  minute
+echo "* * * * * /home/node1/grant_stream_post.sh" >> /etc/crontab
+#edit cron file to execute "get_other_node_ip.sh" every  minute
+echo "* * * * * /home/node1/get_other_node_ip.sh" >> /etc/crontab
+
+apt-get install ipcalc
 
 
 
--- send_ip_maste_wallet.sh
+++ send_ip_maste_wallet.sh
@@ -1,30 +1,5 @@
-#/bin/bash
+#/bin/sh
 
-# send ip to all nodes (their are located in same network)
-# the port is 5000
+# send wallet to master node with telnet on port 1234
 
-# get ip of master node on a mimimalist debian 11 (use ip a)
-my_ip =$(ip a | grep "inet 192.168." | awk '{print $2}' | cut -d "/" -f 1)
-
-# find all nodes in the network 192.168.0.0/16
-for ip in $(seq 1 254); do
-    # print to see the progress
-    echo $ip
-    for ip2 in $(seq 1 254); do
-        # check if ping is ok
-        ping -c 1 192.168.$ip.$ip2 > /dev/null
-        # if ping is ok send ip to port 5000
-        echo $ip2
-        if [ $? -eq 0 ]; then
-            echo $my_ip 
-        fi
-
-        echo $my_ip | nc -w 1 192.168.0.$ip 5000
-    
-    done
-    
-done
-
-
-
-
+# get the ip address of the master node
