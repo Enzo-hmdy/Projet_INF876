0dbfb83a2427eda6c6f86b0e944993fca4f6fe02
pleijan <pierre.tom@outlook.com>
2022-12-06 12:49:54 -0500

    nettoyage projet

--- src/app/Script/get_publish_article.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-# this script is used to get all the articles published in the wikichain stream
-# the articles are stored in the file articles.txt
-
-multichain-cli wikichain liststreamitems wikichain > articles.json
-python3 extract_json.py
-
-
-
--- src/app/Script/send_ip_maste_wallet.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-# send wallet to master node with telnet on port 1234
-
-# get the ip address of the master node
