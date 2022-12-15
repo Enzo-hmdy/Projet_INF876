a61a1589c39f8151c81062c609010c19ea599a04
ehoummad <enzo.hoummady@insa-cvl.fr>
2022-12-15 14:04:56 -0500

    change install master, now works

--- Script enzo/get_other_node_ip.sh
+++ Script enzo/get_other_node_ip.sh
@@ -8,5 +8,5 @@ export NET_ADDR=$net_addr
 # Save the output to the file "addresses.txt"
 nmap -sn $net_addr | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -Ev '(.2|.254)$' | tee ip_addresses.txt
 # IP_master is the first IP address in the file
-IP_master=$(head -n 1 addresses.txt)
+IP_master=$(head -n 1 ip_addresses.txt)
 export IP_master=$IP_master
--- Script enzo/install_master.sh
+++ Script enzo/install_master.sh
@@ -4,13 +4,15 @@ wget https://www.multichain.com/download/multichain-2.3.1.tar.gz
 tar -xvzf multichain-2.3.1.tar.gz
 cd multichain-2.3.1
 mv multichaind multichain-cli multichain-util /usr/local/bin
+cd ..
 multichain-util create wikichain
 # edit "/home/node1/.multichain/wikichain/params.dat" and put every false to true with sed
 sed -i 's/false/true/g' /home/node1/.multichain/wikichain/params.dat
-apt-get install nmap
-apt-get install ipcalc
+apt-get install nmap -y
+apt-get install ipcalc -y
 multichaind wikichain -daemon
 multichain-cli wikichain create stream wikichain true
+multichain-cli wikichain subscribe wikichain
 #edit cron file to execute "get_other_node_ip.sh" every  minute
 echo "* * * * * /home/node1/get_other_node_ip.sh" >> /etc/crontab
 ip_addr=$(ip addr show | grep ens33 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | awk '{print $2}')
@@ -21,7 +23,7 @@ mkdir articles
 echo "* * * * * /home/node1/get_published_articles.sh" >> /etc/crontab
 # activate cron 
 service cron start
-apt-get install jq
+apt-get install jq -y
 
 
 
