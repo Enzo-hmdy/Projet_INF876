d4206791cb54dbee2f5c350d1c5f6e44d8edb990
ehoummad <enzo.hoummady@insa-cvl.fr>
2022-12-15 17:40:59 -0500

    work this time

--- Script enzo/publish.sh
+++ Script enzo/publish.sh
@@ -1,38 +1,11 @@
-#/bin/bash
+# Set the variables for the article
+AUTHOR="John Doe"
+TITLE="My First Article"
+THEME="Technology"
+CONTENT="This is the content of my first article"
 
-# This script is used to publish in the wikichain stream a new article
-# The article is a json file that contains the title, the content and the author of the article and theme
-# The article is published in the wikichain stream
+# Create the JSON string for the article
+JSON_STRING="{\"json\":{\"author\":\"${AUTHOR}\",\"title\":\"${TITLE}\",\"theme\":\"${THEME}\",\"content\":\"${CONTENT}\"}}"
 
-# the user will do in the terminal : ./publish.sh article.json
-# the article.json file will be the first argument of the script
-
-# title is argv[1]
-title=$1
-# encapulate the title in double quotes
-
-
-# content is argv[2]
-content=$2
-# encapulate the content in double quotes
-
-
-# author is argv[3]
-author=$3
-# encapulate the author in double quotes
-
-
-# theme is argv[4]
-theme=$4
-# encapulate the theme in double quotes
-
-
-
-
-# publish the article in the wikichain stream 
-multichain-cli wikichain publish wikichain wikikey "{""json"":{""title"":"$title",""content"":"$content",""author"":"$author",""theme"":"$theme"}}"
-
-
-
-
-
+# Publish the article to the "articles" stream in Multichain
+multichain-cli wikichain publish wikichain "key1" $JSON_STRING 
